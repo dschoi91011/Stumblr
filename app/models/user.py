bac10d6ad0db98f19174a124b1bde756d145be71
Daniel <daniel.choi91011@gmail.com>
2024-05-29 16:08:18 -0700

    debugged user login

--- app/models/user.py
+++ app/models/user.py
@@ -10,6 +10,8 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
@@ -23,11 +25,16 @@ class User(db.Model, UserMixin):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        if len(self.password) < 20:
+            return self.password == password
+        else:
+            return check_password_hash(self.password, password)
 
     def to_dict(self):
         return {
             'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
             'username': self.username,
             'email': self.email
         }
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,18 +3,57 @@ from sqlalchemy.sql import text
 
 
 # Adds a demo user, you can add other users here if you want
-def seed_users():
-    demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+# def seed_users():
+#     demo = User(
+#         first_name='Demo', last_name='User', username='Demo', email='demo@aa.io', hashed_password='password')
+#     marnie = User(
+#         first_name='Marnie', last_name='Johnson', username='marnie', email='marnie@aa.io', hashed_password='password')
+#     bobbie = User(
+#         first_name='Bobbie', last_name='Lee', username='bobbie', email='bobbie@aa.io', hashed_password='password')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+#     db.session.add(demo)
+#     db.session.add(marnie)
+#     db.session.add(bobbie)
+#     db.session.commit()
+
+
+def seed_users():
+    all_users = [
+        {
+            "first_name": "Demo",
+            "last_name": "User",
+            "username": "demouser",
+            "email": "demouser@email.com",
+            "hashed_password": "password1"
+        },
+        {
+            "first_name": "John",
+            "last_name": "Smith",
+            "username": "johnsmith123",
+            "email": "johnsmith123@email.com",
+            "hashed_password": "password2"
+        },
+        {
+            "first_name": "Sarah",
+            "last_name": "Carter",
+            "username": "sarahcarter123",
+            "email": "sarahcarter123@email.com",
+            "hashed_password": "password3"
+        },
+        {
+            "first_name": "David",
+            "last_name": "Kim",
+            "username": "davidkim123",
+            "email": "davidkim123@email.com",
+            "hashed_password": "password4"
+        }
+    ]
+
+    create_users = [User(**user) for user in all_users]
+    add_users = [db.session.add(user) for user in create_users]
     db.session.commit()
+    return create_users
+
 
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
