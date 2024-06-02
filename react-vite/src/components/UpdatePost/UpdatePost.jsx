b929d2798c48d098a3b28781f0eb49505afa632c
Daniel <daniel.choi91011@gmail.com>
2024-06-01 19:05:16 -0700

    updated npm build issue

--- react-vite/src/components/UpdatePost/UpdatePost.jsx
+++ react-vite/src/components/UpdatePost/UpdatePost.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 import {getPostByIdThunk, updatePostThunk} from '../../redux/posts';
 import {getAllPostsThunk} from '../../redux/posts';
