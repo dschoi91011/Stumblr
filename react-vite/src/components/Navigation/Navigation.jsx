c2f1389582a598247a4e5161e1bd31def660bd64
Daniel <daniel.choi91011@gmail.com>
2024-06-07 00:46:27 -0700

    further css

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,50 +1,11 @@
-// import {NavLink, useNavigate} from "react-router-dom";
-// import ProfileButton from "./ProfileButton";
-// import {useSelector} from "react-redux";
-// import OpenModalButton from "../OpenModalButton";
-// import NewPostType from "../NewPostType";
-// import "./Navigation.css";
-
-// function Navigation(){
-//   const navigate = useNavigate();
-//   const currentUser = useSelector(state => state.session.user);
-
-//   const navHomeClick = () => {
-//     navigate('/');
-//   }
-
-//   return(
-//     <div id='nav-bar'>
-//       <div className='nav-upper'>
-//         <NavLink id='stumblr-title' to='/'>stumblr</NavLink>
-
-//         <div className='nav-upper-btn-cluster'>
-//           <ProfileButton className='prof-btn'/>
-//           <button className='nav-home-btn' style={{height: '30px', width: '130px', borderRadius: '10px', cursor: 'pointer'}} onClick={navHomeClick}>
-//             <img style={{height: '25px', width: '25px'}} src='/home_icon.png' alt='profile'/>
-//             <span style={{fontSize: '15px'}}>Home</span>
-//           </button>
-//         </div>
-//       </div>
-//         {
-//             currentUser &&
-//             (<OpenModalButton className='new-post-type' buttonText='Create Post' modalComponent={<NewPostType/>}/>)
-//         }
-//     </div>
-//   );
-// }
-
-// export default Navigation;
-
-
-import { NavLink, useNavigate } from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import ProfileButton from "./ProfileButton";
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import NewPostType from "../NewPostType";
 import "./Navigation.css";
 
-function Navigation() {
+function Navigation(){
   const navigate = useNavigate();
   const currentUser = useSelector(state => state.session.user);
 
@@ -52,22 +13,22 @@ function Navigation() {
     navigate('/');
   }
 
-  return (
+  return(
     <div id='nav-bar'>
       <div className='nav-upper'>
         <NavLink id='stumblr-title' to='/'>stumblr</NavLink>
 
         <div className='nav-upper-btn-cluster'>
           <ProfileButton className='prof-btn'/>
-          <button className='nav-home-btn' style={{ height: '30px', width: '130px', borderRadius: '10px', cursor: 'pointer' }} onClick={navHomeClick}>
-            <img style={{ height: '25px', width: '25px' }} src='/home_icon.png' alt='profile'/>
-            <span style={{ fontSize: '15px' }}>Home</span>
+          <button className='nav-home-btn' style={{height: '30px', width: '130px', borderRadius: '10px', cursor: 'pointer'}} onClick={navHomeClick}>
+            <img style={{height: '25px', width: '25px'}} src='/home_icon.png' alt='profile'/>
+            <span style={{fontSize: '15px'}}>Home</span>
           </button>
         </div>
       </div>
       {
         currentUser &&
-        (<OpenModalButton className='new-post-type' buttonText='Create Post' modalComponent={<NewPostType />} />)
+        (<OpenModalButton className='new-post-type' buttonText='Create Post' modalComponent={<NewPostType/>}/>)
       }
     </div>
   );
--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -1,50 +1,20 @@
-// import { useEffect, useState } from "react";
-// import { Outlet } from "react-router-dom";
-// import { useDispatch } from "react-redux";
-// import { ModalProvider, Modal } from "../context/Modal";
-// import { thunkAuthenticate } from "../redux/session";
-// import Navigation from "../components/Navigation/Navigation";
-
-// import './Layout.css'
-
-// export default function Layout() {
-//   const dispatch = useDispatch();
-//   const [isLoaded, setIsLoaded] = useState(false);
-//   useEffect(() => {
-//     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
-//   }, [dispatch]);
-
-//   return (
-//     <>
-//       <ModalProvider>
-//         <div className="vertical-align">
-//           <Navigation/>
-//           {isLoaded && <Outlet/>}
-//           <Modal/>
-//         </div>
-//       </ModalProvider>
-//     </>
-//   );
-// }
-
-
-import { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { ModalProvider, Modal } from "../context/Modal";
-import { thunkAuthenticate } from "../redux/session";
-import Navigation from "../components/Navigation/Navigation";
+import {useEffect, useState} from "react";
+import {Outlet} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {ModalProvider, Modal} from "../context/Modal";
+import {thunkAuthenticate} from "../redux/session";
+import Navigation from "../components/Navigation";
 
 import './Layout.css'
 
-export default function Layout() {
+export default function Layout(){
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(thunkAuthenticate()).then(() => setIsLoaded(true));
   }, [dispatch]);
 
-  return (
+  return(
     <ModalProvider>
       <div className="layout">
         <Navigation />
