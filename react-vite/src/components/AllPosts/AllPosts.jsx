2851eb959de4e47eb058fc5484ab7a6a33cd81a1
Daniel <daniel.choi91011@gmail.com>
2024-06-09 21:32:48 -0700

    minor change

--- react-vite/src/components/AllPosts/AllPosts.jsx
+++ react-vite/src/components/AllPosts/AllPosts.jsx
@@ -195,7 +195,7 @@ return(
             </div>
 
             <div className='post-block-right'>
-                <h3 className='post-block-right-title'>Featured Posts</h3>
+                <h3 className='post-block-right-title'>Check out these posts</h3>
                 <div className='post-block-right-list'>
                     {isLoaded && randomPosts.map(obj => (
                         <div className='post-block-right-features' key={obj.id} onClick={() => handleSidebarClick(obj.id)} style={{cursor: 'pointer'}}>
