8373e63d1273b5ff245c6e16cf33cc53c56a1473
Daniel <daniel.choi91011@gmail.com>
2024-06-25 16:42:45 -0700

    debugging render issues

---
+++ migrations/versions/20240625164018_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a1ac1883a649
+Revises: 5d50fd70f148
+Create Date: 2024-06-25 16:40:18.577158
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a1ac1883a649'
+down_revision = '5d50fd70f148'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
