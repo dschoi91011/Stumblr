ddaf44bc511e183b3865bc49828ccac628462f4b
Daniel <daniel.choi91011@gmail.com>
2024-06-25 23:47:26 -0700

    db update

---
+++ migrations/versions/20240625234701_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: a1ac1883a649
+Revises: feda5571d99a
+Create Date: 2024-06-25 23:47:01.989537
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a1ac1883a649'
+down_revision = 'feda5571d99a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
